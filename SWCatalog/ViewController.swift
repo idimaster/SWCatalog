d479c1291704191fdf833438e8cb61938ce82e5c
Dmitry Izumskiy <Dmitry_Izumskiy@intuit.com>
2020-10-22 14:11:03 -0700

    Initial Commit

---
+++ SWCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWCatalog
+//
+//  Created by Izumskiy, Dmitry on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
